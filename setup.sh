34e395ea6c7a1e4c69cc4276f17bea75c3b56f99
arj <Andreas.ravnholt99@gmail.com>
2024-11-27 08:23:15 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -1,41 +1,48 @@
 #! /usr/bin/env bash
 
+#Update apt repo
+apt update 
+echo 'Apt repo has been updated'
+
 #Install tldr
-snap install tldr
+snap install tldr 
 echo 'Tldr has been installed succesfully'
 
 #Install ranger
-apt install ranger
+apt install ranger 
 echo 'Ranger has been installed succesfully'
 
 #Install Zoxide
-apt install zoxide
+apt install zoxide 
 echo 'Zoxide (z) has been installed succesfully'
 
 #Install Vim
-apt install vim
+apt install vim 
 echo 'Vim has been installed succesfully'
 
 #Install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-install lazygit -D -t /usr/local/bin/
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') 
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" 
+tar xf lazygit.tar.gz lazygit 
+install lazygit -D -t /usr/local/bin/ 
+rm lazygit.tar.gz 
+rm lazygit 
+echo 'Lazygit has been installed'
 
 #Copy bash aliases
-cp .bash_aliases /home/$SUDO_USER/
-cp .bashrc /home/$SUDO_USER/
-. /home/$SUDO_USER/.bashrc
+cp .bash_aliases /home/$SUDO_USER/ 
+cp .bashrc /home/$SUDO_USER/ 
+. /home/$SUDO_USER/.bashrc 
 echo 'Bash aliases updated succesfully'
 
 #Configure vim setup
-cp .vimrc /home/$SUDO_USER/
-if [ ! -d "/home/$SUDO_USER/.vim/bundle"]; then
-    mkdir /home/$SUDO_USER/.vim/bundle
+cp .vimrc /home/$SUDO_USER/ 
+if [ ! -d "/home/$SUDO_USER/.vim/bundle" ]; then
+    mkdir /home/$SUDO_USER/.vim/bundle 
 fi
-cp ycm_extra_conf.py /home/$SUDO_USER/.vim/
-sudo -u "$SUDO_USER" vim +PluginInstall +qall
-apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
-cd /home/$SUDO_USER/.vim/bundle/YouCompleteMe
-sudo -u "$SUDO_USER" python3 install.py --all
+cp ycm_extra_conf.py /home/$SUDO_USER/.vim/ 
+sudo -u "$SUDO_USER" vim +PluginInstall +qall 
+apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm 
+cd /home/$SUDO_USER/.vim/bundle/YouCompleteMe 
+sudo -u "$SUDO_USER" python3 install.py --all 
 echo 'Vim has been configured successfully'
