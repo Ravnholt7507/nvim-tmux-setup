e12008be10a2b4742a3f73242d786bdca39a56c1
arj <Andreas.ravnholt99@gmail.com>
2025-03-12 13:03:03 +0100

    made background transparent

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,3 +4,10 @@ function ColorMyPencils(color)
 end
 
 ColorMyPencils()
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
