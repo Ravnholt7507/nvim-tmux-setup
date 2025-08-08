817d289bbb8a64c32ef9ce2541eefd9776db1aa2
Andreas Ravnhot Jensen <Andreas.ravnholt@sky.uk>
2025-08-08 09:22:32 +0200

    removed ugly command line (noice)

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -83,10 +83,4 @@ return require('packer').startup(function(use)
         branch = "harpoon2",
         requires = { { "nvim-lua/plenary.nvim" } }
     }
-    use {
-        "folke/noice.nvim",
-        requires = {"MunifTanjim/nui.nvim",
-                    "rcarriga/nvim-notify"}
-    }
-
 end)
