2adf00706661a9d9cbd08027aaa483fcc2151548
Andreas <Andreas.ravnholt99@gmail.com>
2025-12-10 19:56:38 +0100

    made nvimtree show gitignore files

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,1 +1,9 @@
-require("nvim-tree").setup()
+local function on_attach(bufnr)
+    local api = require "nvim-tree.api"
+    api.config.mappings.default_on_attach(bufnr)
+    api.tree.toggle_gitignore_filter()
+end
+
+require("nvim-tree").setup {
+    on_attach = on_attach
+}
